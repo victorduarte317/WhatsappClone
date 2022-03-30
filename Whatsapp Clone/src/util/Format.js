0b22a6c73a9614db6a03769f32d61c34c17d28bd
Victor Duarte <83683867+victorduarte317@users.noreply.github.com>
2022-03-30 18:56:11 -0300

    Add files via upload

--- Whatsapp Clone/src/util/Format.js
+++ Whatsapp Clone/src/util/Format.js
@@ -1,6 +1,6 @@
 // métodos estáticos
 
-class Format {
+export class Format {
     static getCamelCase(text) {
 
     // temos como funçao nativa do javascript formatar o dataset pra camelCase.
@@ -16,5 +16,21 @@ class Format {
         // aqui, ele vai pegar o elemento div, pegar o firstChild - que é o div criado acima e retornar um array de dataset
         // porém, como ele só deve retornar um, o [0] determina isso.
         return Object.keys(div.firstChild.dataset)[0];
+    }
+
+    static toTime(duration) {
+
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        if (hours > 0) {
+
+            // padstart passa 2 casas e completa elas com 0
+            return `${hours}:${minutes}:${seconds.toString().padStart(2, '0')}`;
+        } else {
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+        }
+
     }
 }
