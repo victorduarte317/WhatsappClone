c5c13a3f24da9f2d3ebb5f13dba778772428303f
Victor Duarte <83683867+victorduarte317@users.noreply.github.com>
2022-03-30 18:56:36 -0300

    Update app.js

--- Whatsapp Clone/src/app.js
+++ Whatsapp Clone/src/app.js
@@ -1,2 +1,8 @@
+// agora usando o webpack, é preciso importar e exportar os arquivos pra que ele funcione.
+import {WhatsAppController} from './controller/WhatsAppController'; // importa de whatsappcontroller
+
 window.app = new WhatsAppController();
 // coloca no window pra ser acessado pelo console
+
+// cada classe que você cria, você exporta
+// toda classe que você usa, você importa
