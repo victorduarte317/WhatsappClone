35da96b51d835edbb5149164e0d9ce23395300c7
Victor Duarte <victor.duarte.clear@gmail.com>
2022-04-05 16:10:59 -0300

    tratamento de display no evento do envio e do pós envio de uma imagem

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -606,6 +606,8 @@ export class WhatsAppController{ // vai exportar essa classe pro app.js
 
         this.el.btnSendPicture.on('click', (e)=>{
 
+            this.el.btnSendPicture.disabled = true;
+
             let regex = /^data:(.+);base64,(.*)$/  // expressão regular. Começa em ^, termina em $. Ou seja, começa em image/png, termina em base64
             // vai procurar essa expressao dentro do regex
 
@@ -617,9 +619,43 @@ export class WhatsAppController{ // vai exportar essa classe pro app.js
 
             let filename = `camera${Date.now()}.${ext}`; // filename vai pegar a data de agora
 
-            fetch(this.el.pictureCamera.src)
+            let picture = new Image();
+            picture.src = this.el.pictureCamera.src;
+            picture.onload = (e)=>{
+
+                let canvas = document.createElement('canvas');
+                let context = canvas.getContext('2d');
+
+                canvas.width = picture.width;
+                canvas.height = picture.height;
+
+                context.translate(picture.width, 0);
+                context.scale(-1, 1);
+
+                context.drawImage(picture, 0, 0, canvas.width, canvas.height);
+
+                fetch(canvas.toDataURL(mimeType))
+
                 .then(res => { return res.arrayBuffer(); })
-                .then (buffer => { return new File([buffer], filename, )})
+                .then (buffer => { return new File([buffer], filename, { type: mimeType }); })
+                .then (file =>{
+                    Message.sendImage(this._activeContact.chatId, this._user.email, file);
+
+                    this.el.btnSendPicture.disabled = false;
+
+                    this.closeAllMainPanels();
+                    this._camera.stop();
+                    this.el.btnReshootPanelCamera.hide();
+                    this.el.pictureCamera.hide();
+                    this.el.videoCamera.show();
+                    this.el.containerSendPicture.hide();
+                    this.el.containerTakePicture.show();   
+                    this.el.panelMessagesContainer.show();
+
+                });
+
+            }
+
         });
 
 
